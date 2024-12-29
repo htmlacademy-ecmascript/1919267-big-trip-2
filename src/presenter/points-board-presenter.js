0443624088ebd26035c944519ebf3d9048b0f718
Tatyana Nemchina <23angorka07@gmail.com>
2024-12-29 14:13:47 +0300

    code refactoring

--- src/presenter/points-board-presenter.js
+++ src/presenter/points-board-presenter.js
@@ -1,5 +1,4 @@
 import { render } from '../framework/render.js';
-import PointEditView from '../view/point-edit-view.js';
 import PointItemView from '../view/point-item-view.js';
 import PointsListView from '../view/points-list-view.js';
 import TripSortView from '../view/trip-sort-view.js';
@@ -14,16 +13,9 @@ export default class PointsBoardPresenter {
   }
 
   init() {
-    this.boardPoints = [...this.pointsModel.getPoints()];
+    this.boardPoints = [...this.pointsModel.points];
     render(this.tripSortComponent, this.pointsBoardContainer);
     render(this.pointsListComponent, this.pointsBoardContainer);
-    render(new PointEditView({
-      point: this.boardPoints[0],
-      offers: this.pointsModel.getOffersByType(this.boardPoints[0].type),
-      checkedOffers: [...this.pointsModel.getOffersById(this.boardPoints[0].type, this.boardPoints[0].offers)],
-      destination: this.pointsModel.getDestinationById(this.boardPoints[0].destination),
-      destinations: this.pointsModel.getDestinations()
-    }), this.pointsListComponent.element);
 
     for (let i = 0; i < this.boardPoints.length; i++) {
       render(new PointItemView({
