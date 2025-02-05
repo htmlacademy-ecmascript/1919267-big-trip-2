dec490a38e4843366dad83469327c44a21972946
Tatyana Nemchina <23angorka07@gmail.com>
2025-02-05 17:55:33 +0300

    fixed linter review errors

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,14 +5,6 @@ import durationPlugin from 'dayjs/plugin/duration';
 dayjs.extend(minMax);
 dayjs.extend(durationPlugin);
 
-const MSEC_IN_SEC = 1000;
-const SEC_IN_MIN = 60;
-const MIN_IN_HOUR = 60;
-const HOUR_IN_DAY = 24;
-
-const MSEC_IN_HOUR = MSEC_IN_SEC * SEC_IN_MIN * MIN_IN_HOUR;
-const MSEC_IN_DAY = MSEC_IN_HOUR * HOUR_IN_DAY;
-
 /**
  * Функция, возвращающая отформатированную дату
  * @param {dayjs.ConfigType} date
